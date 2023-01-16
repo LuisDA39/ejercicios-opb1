9a50379c0e4597b54c734ada828b1fd971d8547e
LuisDA39 <107770749+LuisDA39@users.noreply.github.com>
2023-01-16 15:24:52 -0600

    Create ejerciciot1.java

---
+++ JavaBásico/ejerciciot1.java
@@ -0,0 +1,35 @@
+package Ejercicios;
+
+public class Tipos {
+    public static void main(String[] args) {
+
+        // Enteros
+        byte variable1 = 12;
+        System.out.println(variable1);
+        short variable2 = 234;
+        System.out.println(variable2);
+        int variable3 = 13876;
+        System.out.println(variable3);
+        long variable4 = 230465;
+        System.out.println(variable4);
+
+        // Flotante
+        float variable5 = 12.6f;
+        System.out.println(variable5);
+        double variable6 = 3512.431221;
+        System.out.println(variable6);
+
+        // Boolean
+        boolean variable7 = true;
+        System.out.println(variable7);
+        boolean variable8 = false;
+        System.out.println(variable8);
+
+        // Texto
+        char variable9 = 'A';
+        System.out.println(variable9);
+        String variable10 = "Hola Mundo";
+        System.out.println(variable10);
+
+    }
+}
