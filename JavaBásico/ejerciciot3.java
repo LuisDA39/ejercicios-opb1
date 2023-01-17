f0536b964c19fb45ca6e74cc483c9358b027a267
LuisDA39 <107770749+LuisDA39@users.noreply.github.com>
2023-01-16 18:57:46 -0600

    Create ejerciciot3.java

---
+++ JavaBásico/ejerciciot3.java
@@ -0,0 +1,13 @@
+package Ejercicios;
+
+public class Estructuras {
+
+    public static void main(String[] args) {
+        String[] nombres = {"Pedro", "Juan", "Rodrigo", "Matias", "Jorge"};
+
+        for (String nombre : nombres) {
+            System.out.print(nombre + " ");
+        }
+        
+    }
+}
